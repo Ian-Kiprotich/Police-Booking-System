980cffb0f19b8b2473a8e51675866daaf4d4eca0
Ian Kiprotich <90966528+Ian-Kiprotich@users.noreply.github.com>
2023-04-05 10:28:28 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,48 @@
+ <!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kenya Police Service</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-theme.min.css">
+
+     <!-- Custom CSS -->
+    <link href="assets/css/simple-sidebar.css" rel="stylesheet">
+    <link href="assets/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <style>
+  img {
+    display: block;
+    margin-left: 50px;
+    margin-right: 100px;
+    margin-top:125px;
+    margin-bottom: 200px;
+  }
+  </style>
+
+
+  </head>
+<body>
+
+<nav class="navbar navbar-inverse" style="background-color:blue;color: white; margin-bottom:30px;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"> <img  src="admin/img/logo.png" class="rounded" style="width:50%"width="300" height="375" ></a>
+
+    <style>
+        marquee {
+          width: 100%;
+          padding: 10px 0;
+          color:blue;
+          font-weight: bold;
+          font-size: 25px;
+          background-color: cyan;
+        }
+      </style>
+      <marquee direction="scroll">KENYA POLICE SERVICE -UTUMISHI KWA WOTE _POLICE HOT LINES_999 / 112 / 911
+
+</marquee>
+  </div>
+</nav>
